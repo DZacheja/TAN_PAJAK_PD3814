cb91c6067ed6d52c05b313aad9a400b50c94ae97
Damian Laptop <zacheja.damian@gamil.com>
2022-12-03 20:26:14 +0100

    Add project files.

---
+++ 1 - Numer telefonu na stronie/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1___Numer_telefonu_na_stronie
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string website;
+            Console.WriteLine("Podaj adres strony internetowej, lub zostaw domyślny aby wyszukać numery na stronie uczelni.");
+            website = Console.ReadLine();
+            if(string.IsNullOrEmpty(website))
+            {
+                website = @"https://pja.edu.pl/";
+            }
+
+            Console.WriteLine("Wybrano stronę: " + website);
+
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(website);
+                string pageContent = await response.Content.ReadAsStringAsync();
+
+                /*
+                 * 
+                 */
+                string RegEx = @"\+48\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}\s[0-9]{3}|" +
+                                   @"\+48\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}|\+48\s[0-9]{9}|" +
+                                    @"[0-9]{2}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}|" +
+                                    @"[0-9]{2}\s[0-9]{2}\s[0-9]{3}\s[0-9]{2}";
+
+                MatchCollection phoneNumbersInPage = Regex.Matches(pageContent, RegEx);
+
+                Console.WriteLine("Znalezione numery: ");
+
+                foreach (var phoneNumber in phoneNumbersInPage)
+                {
+                    Console.WriteLine(phoneNumber);
+                }
+
+
+            } catch (Exception)
+            {
+                Console.WriteLine("Podana strona jest nieprawidłowa!");
+            }
+        }
+    }
+}
